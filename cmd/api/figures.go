862cf045480cbc04c9493fc0555126fcc7499070
KZhambyl <k_zhambyl@kbtu.kz>
2024-03-09 11:17:09 +0500

    Golang Midterm Project

---
+++ cmd/api/figures.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/KZhambyl/HistoricalFigures/internal/model"
+	"github.com/KZhambyl/HistoricalFigures/internal/validator"
+	"net/http"
+)
+
+// Add a createMovieHandler for the "POST /v1/movies" endpoint.
+func (app *application) createFigureHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name        string `json:"name"`
+		YearsOfLife string `json:"years_of_life"`
+		Description string `json:"description"`
+		Version     int32  `json:"version"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the input struct to a new Movie struct.
+	figure := &data.Figure{
+		Name:        input.Name,
+		YearsOfLife: input.YearsOfLife,
+		Description: input.Description,
+		Version:     input.Version,
+	}
+	// Initialize a new Validator.
+	v := validator.New()
+
+	if data.ValidateFigure(v, figure); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Call the Insert() method on our movies model, passing in a pointer to the
+	// validated movie struct. This will create a record in the database and update the
+	// movie struct with the system-generated information.
+	err = app.models.Figures.Insert(figure)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	// When sending a HTTP response, we want to include a Location header to let the
+	// client know which URL they can find the newly-created resource at. We make an
+	// empty http.Header map and then use the Set() method to add a new Location header,
+	// interpolating the system-generated ID for our new movie in the URL.
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/figures/%d", figure.ID))
+	// Write a JSON response with a 201 Created status code, the movie data in the
+	// response body, and the Location header.
+	err = app.writeJSON(w, http.StatusCreated, envelope{"figure": figure}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) showFigureHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	figure, err := app.models.Figures.Get(id)
+	if err != nil {
+		if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.notFoundResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, err)
+			}
+			return
+		}
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"figure": figure}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) updateFigureHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the movie ID from the URL.
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	// Fetch the existing movie record from the database, sending a 404 Not Found
+	// response to the client if we couldn't find a matching record.
+	figure, err := app.models.Figures.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Declare an input struct to hold the expected data from the client.
+	var input struct {
+		Name        string `json:"name"`
+		YearsOfLife string `json:"years_of_life"`
+		Description string `json:"description"`
+	}
+	// Read the JSON request body data into the input struct.
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	figure.Name = input.Name
+	figure.YearsOfLife = input.YearsOfLife
+	figure.Description = input.Description
+
+	v := validator.New()
+	if data.ValidateFigure(v, figure); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	// Pass the updated movie record to our new Update() method.
+	err = app.models.Figures.Update(figure)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	// Write the updated movie record in a JSON response.
+	err = app.writeJSON(w, http.StatusOK, envelope{"figure": figure}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) deleteFigureHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the movie ID from the URL.
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	// Delete the movie from the database, sending a 404 Not Found response to the
+	// client if there isn't a matching record.
+	err = app.models.Figures.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Return a 200 OK status code along with a success message.
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "figure successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
